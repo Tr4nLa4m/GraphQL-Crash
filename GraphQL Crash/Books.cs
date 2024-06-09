c96412b68591dbbbebc1b429844739e91dd30cd6
Tr4nLa4m <tranlamiter@gmail.com>
2024-06-09 11:18:48 +0700

    init

---
+++ GraphQL Crash/Books.cs
@@ -0,0 +1,14 @@
+﻿namespace GraphQL_Crash
+{
+    public class Books
+    {
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ GraphQL Crash/Query.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphQL_Crash
+{
+    public class Query
+    {
+        public Books GetBook() =>
+        new Books
+        {
+            Title = "C# in depth.",
+            Author = new Author
+            {
+                Name = "Jon Skeet"
+            }
+        };
+    }
+}
